d33fc959bcea5bc983cc46be30bf0c12a58cfa69
saya1a <40052644+saya1a@users.noreply.github.com>
2023-10-11 00:49:02 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    profile = "default"
+    region = "us-east-1"
+}
+
+
+#ec2 provision
+resource aws_instance "test-instance" {
+    ami           = "ami-053b0d53c279acc90"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = ["sg-0088786e83e38b36a"]
+    key_name      = "ansi-key"
+
+    tags = {
+    Name = "Test-EC2"
+  }
+}
